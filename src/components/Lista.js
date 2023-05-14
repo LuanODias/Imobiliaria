4832306bf6a3981c8928c4ce8c95a98ebdb84673
LuanODias <luoliveidias@gmail.com>
2023-05-14 01:12:16 -0300

    Melhoria da interface de Listagem

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -1,9 +1,9 @@
-import { Alert, View, Text, ScrollView, Image, Dimensions } from 'react-native'
+import { Alert, View, Text, ScrollView, Image, Dimensions, StyleSheet } from 'react-native'
 import React, { useContext, useState, useEffect } from 'react'
 import ImovelContext from '../context/ImovelContext'
 import { ListItem, Icon } from '@rneui/base'
 import FormularioLocador from './FormularioLocador'
-import { listarImoveis } from '../services/ImovelServices'
+import { deletarImovel, listarImoveis } from '../services/ImovelServices'
 
 
 
@@ -28,16 +28,17 @@ export default props => {
         })
     }, [])
 
-   
-    function remover(imovel) {
+
+    const remover = (imovel) => {
         Alert.alert('Removendo imóvel',
             'Deseja realmente remover este imóvel?',
             [
                 {
                     text: "Sim",
-                    onPress() {
-                        //deletarImovel (imovel)
-                        carregarLista()
+                    async onPress() {
+                        console.warn(imovel.id);
+                        await deletarImovel(imovel.id);
+                        await carregarLista()
                     }
                 },
                 {
@@ -48,25 +49,37 @@ export default props => {
 
     const renderizarTipoVenda = () => {
         return (
-            <Text>Venda</Text>
+            <Text style={styles.subitituloItem}>Venda</Text>
         )
     }
 
     const renderizarTipoAluguel = () => {
         return (
-            <Text>Aluguel</Text>
+            <Text style={styles.subitituloItem}>Aluguel</Text>
         )
     }
 
     const renderizarTipoApartamento = () => {
         return (
-            <Text>Apartamento</Text>
+            <Text style={styles.subitituloItem}>Apartamento</Text>
         )
     }
 
     const renderizarTipoCasa = () => {
         return (
-            <Text>Casa</Text>
+            <Text style={styles.subitituloItem}>Casa</Text>
+        )
+    }
+
+    const RenderizarLocadoTrue = () => {
+        return (
+            <Text style={styles.subitituloItem}>Sim</Text>
+        )
+    }
+
+    const RenderizarLocadoFalse = () => {
+        return (
+            <Text style={styles.subitituloItem}>Não</Text>
         )
     }
 
@@ -75,46 +88,61 @@ export default props => {
             <Text>Loading...</Text>
         )
     } else {
-        console.warn(imoveis);
-        
         return (
             <ScrollView>
                 <View>
                     {
-                        
+
                         imoveis.map(imovel => {
                             return (
                                 <ListItem key={imovel.id}>
                                     <ListItem.Content>
-                                        <Image style={{width: "100%", height: height * 0.20}} source={{uri: imovel.foto !=  "" ? imovel.foto : "https://www.brognoli.com.br/images/fotos/40570/imFt0bkMS_40570645934b3502dc.jpg"}}/>
-                                            <Text>{imovel.endereco}</Text>
-                                            <Text>Tipo de contrato: </Text>
+                                        <Image style={{ width: "100%", height: height * 0.20 }} source={{ uri: imovel.foto != "" ? imovel.foto : "https://www.brognoli.com.br/images/fotos/40570/imFt0bkMS_40570645934b3502dc.jpg" }} />
+                                        <Text style={styles.titulo}>{imovel.endereco}</Text>
+                                        <View style={styles.row}>
+                                            <Text style={styles.subititulo}>Tipo de contrato:</Text>
                                             {imovel.tipoCadastro == 1 && renderizarTipoVenda()}
                                             {imovel.tipoCadastro == 2 && renderizarTipoAluguel()}
-                                            <Text>Tipo do imóvel: </Text>
+                                        </View>
+                                        <View style={styles.row}>
+                                            <Text style={styles.subititulo}>Tipo do imóvel:</Text>
                                             {imovel.tipoImovel == 1 && renderizarTipoApartamento()}
                                             {imovel.tipoImovel == 2 && renderizarTipoCasa()}
+                                        </View>
+                                        <View style={styles.row}>
+                                            <Text style={styles.subititulo}>Número de quartos:</Text>
+                                            <Text style={styles.subitituloItem}>{imovel.numeroQuartos}</Text>
+                                        </View>
+                                        <View style={styles.row}>
+                                            <Text style={styles.subititulo}>Número de banheiros:</Text>
+                                            <Text style={styles.subitituloItem}>{imovel.numeroBanheiros}</Text>
+                                        </View>
 
-                                            {/* {imovel.locado == 'true' && RenderizarLocadoTrue()} */}
-                                            {/* {imovel.locado == 'false' && RenderizarLocadoFalse()} */}
+                                        <View style={styles.row}>
+                                            <Text style={styles.subititulo}>Locado:</Text>
+                                            {imovel.locado == true && RenderizarLocadoTrue()}
+                                            {imovel.locado == false && RenderizarLocadoFalse()}
+                                        </View>
 
-                                        {/* <ListItem.Subtitle>
+
+                                        {/* 
                                     {imovel.nomelocatario == 'null' && renderizarImovelSemLocatario()}
                                     {imovel.nomelocatario != 'null' && renderizarImovelComLocatario(imovel)}
-                                </ListItem.Subtitle> */}
-                                        {/* <ListItem.Title>
-                                    <Text>R$</Text>
-                                    {imovel.tipoContrato == 'Aluguel' && renderizarValorAluguel(imovel)}
-                                    {imovel.tipoContrato == 'Venda' && renderizarValorVenda(imovel)}
-                                </ListItem.Title> */}
-                                <Icon name='edit' onPress={() =>
-                                        props.navigation.navigate("CadastroImovel", imovel)
-                                    } />
-                                    <Icon name='delete' onPress={() => remover(imovel)} />
+                                     */}
+                                        <View style={styles.row}>
+                                            <Text style={styles.titulo}>R$</Text>
+                                            <Text style={styles.titulo}>{imovel.valorAluguel}</Text>
+                                        </View>
+                                        <View style={styles.rowCenter}>
+                                            <Icon name='edit' onPress={() =>
+                                                props.navigation.navigate("CadastroImovel", imovel)
+                                            } />
+                                            <Icon name='delete' onPress={() => remover(imovel)} />
+                                        </View>
                                     </ListItem.Content>
                                     {/* {imovel.tipoContrato == 'Aluguel' && imovel.locado == 'false' && adicionarLocador(imovel.id)} */}
-                                    
-                                    
+
+
                                 </ListItem>
 
                             )
@@ -127,3 +155,32 @@ export default props => {
     }
 
 }
+
+const styles = StyleSheet.create({
+    titulo: {
+        fontSize: 20,
+        color: 'black',
+        marginTop: 3,
+        fontWeight: 600
+    },
+    subititulo: {
+        fontSize: 14,
+        color: 'black',
+        padding: 1,
+        fontWeight: 600
+    },
+    subitituloItem: {
+        fontSize: 14,
+        color: 'black',
+        padding: 1,
+    },
+    row: {
+        flexDirection: 'row'
+    },
+    rowCenter: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: "100%",
+    }
+})
--- src/services/ImovelServices.js
+++ src/services/ImovelServices.js
@@ -23,6 +23,28 @@ export async function salvarImovel(imovel) {
     }
 }
 
+export async function deletarImovel(id) {
+    try {
+        const login = await buscarLogin()
+        const response = await fetch(
+            `http://ec2-54-166-238-5.compute-1.amazonaws.com/imoveis/${id}`,
+            {
+                method: 'DELETE',
+                headers: {
+                    //'Content-type': 'application/json',
+                    'apikey': 'ffefaeb379f5496dc8232d60ce8cf81a30453180',
+                    'token': login.token
+                }
+            }
+        )
+        console.log('Requisição para excluir imóvel realizada com sucesso!')
+        console.log(response)
+    } catch (ex) {
+        console.log('Erro ao excluir imovel!')
+        console.log(ex)
+    }
+}
+
 
 
 export async function listarImoveis() {
@@ -40,7 +62,7 @@ export async function listarImoveis() {
         )
         return response.json()
     } catch (ex) {
-        console.log('Erro ao listar as imoveis!')
+        console.log('Erro ao listar os imoveis!')
         console.log(ex)
     }
 }
